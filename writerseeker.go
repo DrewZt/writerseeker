591bede95230517005d6c4ca67431e1d83388b7f
DrewZt <37324084+DrewZt@users.noreply.github.com>
2023-08-31 12:10:46 +0800

    export buf

--- writerseeker.go
+++ writerseeker.go
@@ -8,29 +8,29 @@ import (
 
 // WriterSeeker is an in-memory io.WriteSeeker implementation
 type WriterSeeker struct {
-	buf bytes.Buffer
+	Buf bytes.Buffer
 	pos int
 }
 
-// Write writes to the buffer of this WriterSeeker instance
+// Write writes to the Buffer of this WriterSeeker instance
 func (ws *WriterSeeker) Write(p []byte) (n int, err error) {
-	// If the offset is past the end of the buffer, grow the buffer with null bytes.
-	if extra := ws.pos - ws.buf.Len(); extra > 0 {
-		if _, err := ws.buf.Write(make([]byte, extra)); err != nil {
+	// If the offset is past the end of the Buffer, grow the Buffer with null bytes.
+	if extra := ws.pos - ws.Buf.Len(); extra > 0 {
+		if _, err := ws.Buf.Write(make([]byte, extra)); err != nil {
 			return n, err
 		}
 	}
 
-	// If the offset isn't at the end of the buffer, write as much as we can.
-	if ws.pos < ws.buf.Len() {
-		n = copy(ws.buf.Bytes()[ws.pos:], p)
+	// If the offset isn't at the end of the Buffer, write as much as we can.
+	if ws.pos < ws.Buf.Len() {
+		n = copy(ws.Buf.Bytes()[ws.pos:], p)
 		p = p[n:]
 	}
 
-	// If there are remaining bytes, append them to the buffer.
+	// If there are remaining bytes, append them to the Buffer.
 	if len(p) > 0 {
 		var bn int
-		bn, err = ws.buf.Write(p)
+		bn, err = ws.Buf.Write(p)
 		n += bn
 	}
 
@@ -38,7 +38,7 @@ func (ws *WriterSeeker) Write(p []byte) (n int, err error) {
 	return n, err
 }
 
-// Seek seeks in the buffer of this WriterSeeker instance
+// Seek seeks in the Buffer of this WriterSeeker instance
 func (ws *WriterSeeker) Seek(offset int64, whence int) (int64, error) {
 	newPos, offs := 0, int(offset)
 	switch whence {
@@ -47,7 +47,7 @@ func (ws *WriterSeeker) Seek(offset int64, whence int) (int64, error) {
 	case io.SeekCurrent:
 		newPos = ws.pos + offs
 	case io.SeekEnd:
-		newPos = ws.buf.Len() + offs
+		newPos = ws.Buf.Len() + offs
 	}
 	if newPos < 0 {
 		return 0, errors.New("negative result pos")
@@ -56,9 +56,9 @@ func (ws *WriterSeeker) Seek(offset int64, whence int) (int64, error) {
 	return int64(newPos), nil
 }
 
-// Reader returns an io.Reader. Use it, for example, with io.Copy, to copy the content of the WriterSeeker buffer to an io.Writer
+// Reader returns an io.Reader. Use it, for example, with io.Copy, to copy the content of the WriterSeeker Buffer to an io.Writer
 func (ws *WriterSeeker) Reader() io.Reader {
-	return bytes.NewReader(ws.buf.Bytes())
+	return bytes.NewReader(ws.Buf.Bytes())
 }
 
 // Close :
@@ -68,5 +68,5 @@ func (ws *WriterSeeker) Close() error {
 
 // BytesReader returns a *bytes.Reader. Use it when you need a reader that implements the io.ReadSeeker interface
 func (ws *WriterSeeker) BytesReader() *bytes.Reader {
-	return bytes.NewReader(ws.buf.Bytes())
+	return bytes.NewReader(ws.Buf.Bytes())
 }
